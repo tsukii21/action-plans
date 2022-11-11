a8be5990ca842ced9deb36196f7ca6a6654a722b
tsukii21 <jaidevkr2@gmail.com>
2022-11-12 02:47:18 +0530

    added mui and initialized theme

---
+++ src/config/theme.js
@@ -0,0 +1,10 @@
+import { createTheme } from "@mui/material"
+import "@fontsource/manrope"
+
+const theme = createTheme({
+  typography: {
+    fontFamily: '"Manrope", sans-serif',
+  },
+})
+
+export default theme
